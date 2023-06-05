d748edca936e9dfe6d43e4a0b0e907d1b2eee832
Павел Трифонов <p4veltr@gmail.com>
2023-06-05 22:53:41 +0300

    Jpa-Repository add

--- src/main/java/com/example/dbdaohibernate/repository/Repository.java
+++ src/main/java/com/example/dbdaohibernate/repository/Repository.java
@@ -1,29 +1,8 @@
 package com.example.dbdaohibernate.repository;
 
 import com.example.dbdaohibernate.model.Person;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.Query;
-import jakarta.transaction.Transactional;
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @org.springframework.stereotype.Repository
-public class Repository {
-
-    @PersistenceContext
-    EntityManager entityManager;
-
-    Person person = new Person();
-
-    @Transactional
-    public void run(Person person) {
-        entityManager.persist(person);
-    }
-
-    @Transactional
-    public List getPersonsByCity(String city) {
-        Query query = entityManager.createQuery("select name from Person where cityOfLiving = :city order by name, surname");
-        query.setParameter("city", city);
-        return query.getResultList();
-    }
+public interface Repository extends JpaRepository<Person, Integer> {
 }
